5709d05fee7d655708a052cea532a32677cc963f
ws_t_afcp <allan@t2s.com.br>
2023-05-03 21:36:28 -0300

    feito mudança a partir da tag

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI();//mudanca a partir da 1.0.0
 }
 
 app.UseHttpsRedirection();
